9acc93287de3df77adb2a5ab6fd3e5bf6b4014bc
zhangxiaoyun1 <2210545262@qq.com>
2019-10-18 16:38:07 +0800

    message

---
+++ precache-manifest.bb81b9eee2395c1579655b9159f40f4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1d8ff237f892564d655661886d8e3ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c477d0fb43d8eab8db1b",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "f56bbd54c07011861250",
+    "url": "/static/css/main.3389489c.chunk.css"
+  },
+  {
+    "revision": "c477d0fb43d8eab8db1b",
+    "url": "/static/js/2.d4db2a69.chunk.js"
+  },
+  {
+    "revision": "f56bbd54c07011861250",
+    "url": "/static/js/main.45109101.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
